3cf2008fa7dc8cd6e7df1ec4a42986b3165bd222
umiyosh <yumino99@gmail.com>
2013-04-21 07:27:06 +0000

    .zshディレクトリが2回移動してしまうのをやめた

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .zshenv .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .gvimrc .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .rvmrc )
+DOT_FILES=( .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .zshenv .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .gvimrc .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .rvmrc )
 
 for file in ${DOT_FILES[@]}
 do
@@ -8,6 +8,10 @@ do
         ln -s $HOME/dotfiles/$file $HOME/$file
     fi
 done
+
+if [[ -d $HOME/.zsh/ ]]; then
+   ln -s $HOME/dotfiles/.zsh $HOME/.zsh
+fi
 
 if [[ -d $HOME/.vim/ ]]; then
     if [[ ! -d $HOME/.vim/dict ]]; then
