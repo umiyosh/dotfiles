8db0d6ddb4964fb70ba698e2e0f15a87850f7002
umiyosh <yumino99@gmail.com>
2015-09-17 02:02:08 +0900

    全部関数にした

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ function deployDotfiles() {
               .dir_colors .rdebugrc .rvmrc .perltidyrc .mackup.cfg
              )
   DOT_DIRS=(.zsh .vim .peco )
+
   # dotfiles
   for file in ${DOT_FILES[@]}
   do
@@ -22,89 +23,109 @@ function deployDotfiles() {
     fi
   done
 
+  # dotdirs
   for directory in ${DOT_DIRS[@]}
   do
     if [[ ! -d $HOME/${directory}/ ]]; then
      ln -s $HOME/dotfiles/${directory} $HOME/${directory}
     fi
   done
+
+  # local dotfile
+  if [[ -e $HOME/Dropbox/.zshrc.local ]]; then
+    ln -s $HOME/Dropbox/conf/.zshrc.local $HOME/.zshrc.local
+  fi
+}
+
+function changeShell() {
+  if [[ ! "$SHELL" =~ .+zsh$ ]]; then
+    sudo chsh -s $(which zsh)
+  fi
+}
+
+function deployLocalBin() {
+  if [[ ! -d $HOME/local/bin ]]; then
+    mkdir -p $HOME/local/bin/
+    ln -s $HOME/dotfiles/bin/git_diff_wrapper $HOME/local/bin/git_diff_wrapper
+    ln -s $HOME/dotfiles/bin/php-xdebug $HOME/local/bin/php-xdebug
+  fi
+}
+
+function deploySnippets() {
+  if [[ ! -d $HOME/.vim/snippets ]]; then
+    git clone https://github.com/umiyosh/snippets.git $HOME/.vim/snippets/
+  fi
+}
+
+function installAntigen() {
+  ## antigen
+  if [[ ! -d $HOME/.zsh/extention/antigen/ ]]; then
+   git clone https://github.com/zsh-users/antigen.git $HOME/.zsh/extention/antigen/
+  fi
+}
+
+function installAutojump() {
+## zsh-autojump
+  if ! autojump --stat 1>/dev/null 2>&1; then
+    case "${OSTYPE}" in
+    darwin*)
+      brew install autojump
+      ;;
+    linux*)
+      git clone https://github.com/joelthelion/autojump.git $HOME/.zsh/extention/autojump
+      cd $HOME/.zsh/extention/autojump/
+      ./install.py
+      cd $HOME/dotfiles/
+      ;;
+    esac
+  fi
+}
+
+function installPeco() {
+  ## peco
+  if ! which peco; then
+    case "${OSTYPE}" in
+    darwin*)
+      brew tap peco/peco
+      brew install peco
+      brew install migemogrep
+      ;;
+    linux*)
+      go get github.com/peco/peco/cmd/peco
+      go get github.com/peco/migemogrep
+      ;;
+    esac
+  fi
+}
+
+function setupVimPlugins() {
+  # vim
+  ## vunndle and BundleInstall and make vimproc
+  if [[ ! -d $HOME/.vim/bundle/vundle/ ]]; then
+    mkdir -p $HOME/.vim/bundle/
+    git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle/
+    vim -Nu $HOME/dotfiles/.vimrc.bundle +BundleInstall! +qall
+    cd $HOME/.vim/bundle/vimproc/
+    case "${OSTYPE}" in
+    darwin*)
+      make -f make_mac.mak
+      ;;
+    linux*)
+      make -f make_unix.mak
+      ;;
+    esac
+    cd $HOME/dotfiles/
+  else
+    vim -Nu $HOME/dotfiles/.vimrc.bundle +BundleInstall +qall
+  fi
 }
 
 deployDotfiles
-
-if [[ -e $HOME/Dropbox/.zshrc.local ]]; then
-  ln -s $HOME/Dropbox/conf/.zshrc.local $HOME/.zshrc.local
-fi
-
-if [[ ! "$SHELL" =~ .+zsh$ ]]; then
-  sudo chsh -s $(which zsh)
-fi
-
-if [[ ! -d $HOME/local/bin ]]; then
-  mkdir -p $HOME/local/bin/
-  ln -s $HOME/dotfiles/bin/git_diff_wrapper $HOME/local/bin/git_diff_wrapper
-  ln -s $HOME/dotfiles/bin/php-xdebug $HOME/local/bin/php-xdebug
-fi
-
-if [[ ! -d $HOME/.vim/snippets ]]; then
-  git clone https://github.com/umiyosh/snippets.git $HOME/.vim/snippets/
-fi
-
-# zsh extention
-## antigen
-if [[ ! -d $HOME/.zsh/extention/antigen/ ]]; then
- git clone https://github.com/zsh-users/antigen.git $HOME/.zsh/extention/antigen/
-fi
-
-## zsh-autojump
-if ! autojump --stat 1>/dev/null 2>&1; then
-  case "${OSTYPE}" in
-  darwin*)
-    brew install autojump
-    ;;
-  linux*)
-    git clone https://github.com/joelthelion/autojump.git $HOME/.zsh/extention/autojump
-    cd $HOME/.zsh/extention/autojump/
-    ./install.py
-    cd $HOME/dotfiles/
-    ;;
-  esac
-fi
-
-## peco
-if ! which peco; then
-  case "${OSTYPE}" in
-  darwin*)
-    brew tap peco/peco
-    brew install peco
-    brew install migemogrep
-    ;;
-  linux*)
-    go get github.com/peco/peco/cmd/peco
-    go get github.com/peco/migemogrep
-    ;;
-  esac
-fi
-## peco
-
-# vim
-## vunndle and BundleInstall and make vimproc
-if [[ ! -d $HOME/.vim/bundle/vundle/ ]]; then
-  mkdir -p $HOME/.vim/bundle/
-  git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle/
-  vim -Nu $HOME/dotfiles/.vimrc.bundle +BundleInstall! +qall
-  cd $HOME/.vim/bundle/vimproc/
-  case "${OSTYPE}" in
-  darwin*)
-    make -f make_mac.mak
-    ;;
-  linux*)
-    make -f make_unix.mak
-    ;;
-  esac
-  cd $HOME/dotfiles/
-else
-  vim -Nu $HOME/dotfiles/.vimrc.bundle +BundleInstall +qall
-fi
-
+changeShell
+deployLocalBin
+deploySnippets
+installAntigen
+installAutojump
+installPeco
+setupVimPlugins
 
