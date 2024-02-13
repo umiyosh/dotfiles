42d78d689ef93080affbc2c8099210bd9da11137
umiyosh <yumino99@gmail.com>
2024-02-13 20:28:51 +0900

    Fix URL encoding in olog.sh script

--- bin/olog.sh
+++ bin/olog.sh
@@ -1,9 +1,9 @@
-
 url_encode() {
-  python -c "import urllib.parse; print(urllib.parse.quote_plus(\"$*\"))"
+  python -c "import urllib.parse; print(urllib.parse.quote(\"$*\"))"
 }
 
 current_time=$(date "+%H:%M")
-memo=$(url_encode "$*")
+
+memo=$(url_encode $(echo "$*" | tr '\n' ' '))
 
 open -g "obsidian://advanced-uri?vault=ObsidianVault&daily=true&mode=append&data=-%20${current_time}%20${memo}"
