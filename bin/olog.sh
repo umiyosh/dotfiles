ca3907e3457ac64ae8307d833fb456a9c964c75e
umiyosh <yumino99@gmail.com>
2024-11-13 17:11:23 +0900

    fix: resolved journaling log issue caused by double escaping

--- bin/olog.sh
+++ bin/olog.sh
@@ -1,9 +1,12 @@
 url_encode() {
-  python -c "import urllib.parse; print(urllib.parse.quote(\"$*\"))"
+  python -c 'import sys, urllib.parse; print(urllib.parse.quote(urllib.parse.quote(sys.argv[1])))' "$*"
 }
 
-current_time=$(date "+%H:%M")
+current_time=$(date "+%H:%M:%S")
+input=$(echo "$*" | tr '\n' ' ')
 
-memo=$(url_encode $(echo "$*" | tr '\n' ' '))
-
+# echo "Input: $input"
+memo=$(url_encode "$input")
+# echo "Memo: $memo"
+# echo "URL: obsidian://advanced-uri?vault=ObsidianVault&daily=true&mode=append&data=-%20${current_time}%20${memo}"
 open -g "obsidian://advanced-uri?vault=ObsidianVault&daily=true&mode=append&data=-%20${current_time}%20${memo}"
