74b0be097e110844e34e0d71408819da65569d08
umiyosh <yumino99@gmail.com>
2025-03-10 02:43:38 +0900

    fix: restore missing keymap settings for Vim operator replace

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -114,6 +114,7 @@ return {
   {
     "kana/vim-operator-replace",
     keys = {
+      { '_', '<Plug>(operator-replace)', mode = '', desc = 'Replace with operator' },
       { 'p', '<Plug>(operator-replace)', mode = 'v', desc = 'Replace with yanked text' },
     },
   },
