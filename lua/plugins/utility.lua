aff451734a6de77a35213f675e3e881bce498ad1
umiyosh <yumino99@gmail.com>
2025-03-20 16:14:25 +0900

    fix(airline): suppress unnecessary warnings by aligning lazy loading order

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -103,7 +103,12 @@ return {
   {
     "vim-airline/vim-airline",
     cond = not vim.g.vscode,
-    event = "VimEnter",
+    -- event = "VimEnter",
+    event = {
+        "InsertEnter",
+        "CmdlineEnter",  -- コマンドライン補完のため
+        "BufReadPre",    -- エラー検出のため
+    },
     init = function()
       vim.g['airline_powerline_fonts'] = 1
       vim.g['airline_theme'] = 'dark'
