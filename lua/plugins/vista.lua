b7aea5cde80eda5c22e39c841d3149db4d18729e
umiyosh <yumino99@gmail.com>
2025-03-26 01:19:31 +0900

    chore(neovim): change lazy load event to very lazy to avoid interference with external FZF searches

--- lua/plugins/vista.lua
+++ lua/plugins/vista.lua
@@ -2,11 +2,12 @@ return {
   {
     -- ソースコード上のメソッド宣言、変数宣言の一覧を表示
     "liuchengxu/vista.vim",
-    keys = {
-      { "<leader>tl", "<cmd>Vista coc<CR>", mode = "n", silent = true },
-    },
+    event = "VeryLazy",
     init = function()
       vim.g['vista#renderer#enable_icon'] = 1
+    end,
+    config = function()
+      vim.keymap.set('n', '<leader>tl', '<cmd>Vista coc<CR>', { silent = true })
     end,
   },
 }
