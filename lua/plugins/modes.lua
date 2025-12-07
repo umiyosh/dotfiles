f227c00b0f406571f510922903b8cb2f2f57b66a
umiyosh <yumino99@gmail.com>
2025-12-07 11:40:39 +0900

    feat(modes.nvim): add color display plugin for neovim mode

---
+++ lua/plugins/modes.lua
@@ -0,0 +1,35 @@
+return {
+  "mvllow/modes.nvim",
+  config = function()
+    require("modes").setup({
+      -- ここは好きな設定で
+      line_opacity = 0.15,
+      set_cursor = true,
+      set_cursorline = true,
+    })
+
+    -- modes.nvim が作った gutter 用ハイライトを自前で戻す
+    local function fix_modes_gutter()
+      for _, mode in ipairs({ "Copy", "Delete", "Insert", "Visual" }) do
+        -- 行番号は通常の CursorLineNr に合わせる
+        vim.api.nvim_set_hl(0, "Modes"..mode.."CursorLineNr", {
+          link = "CursorLineNr",
+        })
+        -- sign / fold カラムは SignColumn / FoldColumn に合わせる
+        vim.api.nvim_set_hl(0, "Modes"..mode.."CursorLineSign", {
+          link = "SignColumn",
+        })
+        vim.api.nvim_set_hl(0, "Modes"..mode.."CursorLineFold", {
+          link = "FoldColumn",
+        })
+      end
+    end
+
+    fix_modes_gutter()
+
+    -- colorscheme 変更時に modes.lua が再定義するので、その後でもう一回潰す
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      callback = fix_modes_gutter,
+    })
+  end,
+}
