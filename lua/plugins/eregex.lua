ff506b54615db1619ab5ace913f5a984a7b20b24
umiyosh <yumino99@gmail.com>
2026-01-16 14:16:17 +0900

    chore(eregex.vim): add G command configuration

--- lua/plugins/eregex.lua
+++ lua/plugins/eregex.lua
@@ -2,7 +2,7 @@ return {
     {
         -- eregex.vim : vimの正規表現をrubyやperlの正規表現な入力でできる :%S/perlregex/
         "othree/eregex.vim",
-        cmd = { "S", "M" },
+        cmd = { "S", "M", "G", "V" },
         init = function()
             vim.g['eregex_forward_delim'] = 'M/'
             vim.g['eregex_backward_delim'] = 'M?'
